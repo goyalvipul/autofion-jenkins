b01c90f12bdaf3d50c1d9e61b99e6016498ce9b5
Abhishek Sharda <abhishek.sharda@pearson.com>
2020-06-10 23:39:17 -0700

    starting new repo

---
+++ src/main/java/foo/App.java
@@ -0,0 +1,40 @@
+package foo;
+/**
+ * Hello world new....!
+ *
+ */
+public class App {
+		
+public String getAccountName(){
+			return "QATester";
+}
+public int  addAccount(int a,int b){
+		
+	int c= a+b;
+		return c;
+	}
+public int  mutiplyAccount(int a,int b){
+	
+	
+	int f= a*b;
+	//  hello this is done
+	return f;
+		
+}
+
+public int  deleteAccount(int a,int b){
+	
+	
+	int c= a-b;
+	
+	return c;
+	
+	
+}
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello all to latest code worlds...!!!" );
+        
+    }
+}
---
+++ src/test/java/foo/AppTest.java
@@ -0,0 +1,90 @@
+package foo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class AppTest{
+	
+	
+	public static WebDriver driver ;
+	
+ App app;
+	@BeforeSuite
+	public void setup(){
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Selenium\\chromedriver.exe");
+		
+		driver= new ChromeDriver();
+		
+		app = new App();
+		
+	}
+	
+	@Test
+	public void login(){
+		
+		driver.get("http://google.com");
+		
+		
+	
+		System.out.println("Insidloginpage");
+		
+		Assert.assertEquals(driver.getTitle(), "Google");
+	}
+	
+	@Test
+	public void verifyAccount(){
+		
+	
+		System.out.println("Inside the the Verify--...");
+		
+		Assert.assertEquals(app.getAccountName(), "QATester");
+	}
+	
+	@Test
+	public void verifyAddAccount(){
+		
+	
+		System.out.println("Inside  account ");
+		
+		Assert.assertEquals(app.addAccount(2, 3), 5);
+	}
+	
+	@Test
+	public void verifyMultiplyAccount(){
+		
+	
+		System.out.println("Inside mutiply account");
+		
+		Assert.assertEquals(app.mutiplyAccount(4, 5),20);
+	}
+	
+	@Test
+	public void verifyDeleteAccount(){
+		
+	
+		System.out.println("Inside Delete  account");
+		
+		Assert.assertEquals(app.deleteAccount(10, 5),5);
+	}
+	
+	
+	
+	@AfterSuite
+	public void tearDown(){
+		
+		driver.close();
+	}
+	
+	
+	
+	
+	
+	
+}
